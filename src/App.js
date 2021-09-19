f9477d00a6a65e80338e527c5c3e36d540eb8540
Sabin Banjara <sbanjara@stu.jsu.edu>
2021-09-19 14:18:40 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main-container">
+      <h1>Markeplace Project</h1>
+      <p>By Divash, Mausam, Nikesh, and Sabin</p>
     </div>
   );
 }
